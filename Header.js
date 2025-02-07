3654b6ecf3e04ad4b3f3c7ee40392c9ec9f29191
Harman604-pixel <kharmanjeet832@gmail.com>
2025-02-07 16:52:42 -0500

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+      </nav>
+    </header>
+  );
+}
