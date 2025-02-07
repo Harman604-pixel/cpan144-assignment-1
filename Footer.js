d9fcd87d38986a04a271a9371f9b2aea1b1307a0
Harman604-pixel <kharmanjeet832@gmail.com>
2025-02-07 16:52:21 -0500

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>&copy; 2023 My Web Application</p>
+    </footer>
+  );
+}
